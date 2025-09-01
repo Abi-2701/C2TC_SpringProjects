45c6ea07b19bda88e8f8bece9121fd48faf0b9e8
Abi-2701 <abiramdil@gmail.com>
2025-09-01 16:29:01 +0530

    Spring Project

---
+++ SpringDI/src/main/java/com/tnsif/SpringDI/App.java
@@ -0,0 +1,13 @@
+package com.tnsif.SpringDI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
