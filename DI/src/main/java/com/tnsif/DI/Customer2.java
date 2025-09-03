798873702efdb4e02b9d6d9f1cd430378b034d7c
Abi-2701 <abiramdil@gmail.com>
2025-09-03 20:21:35 +0530

    Dependency Injection...

---
+++ DI/src/main/java/com/tnsif/DI/Customer2.java
@@ -0,0 +1,30 @@
+package com.tnsif.DI;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer2 {
+	
+	private int id;
+	private String cname;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	public void show()
+	{
+		System.out.println("Customer 2 object printed");
+	}
+	
+}
